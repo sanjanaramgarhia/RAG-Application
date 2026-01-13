886e440ae803298902563f8ec6e1248924e94281
sanjana_tech <sanjanaramgarhia@gmail.com>
2026-01-13 10:26:00 +0530

    Update search.py

--- search.py
+++ search.py
@@ -1,5 +1,4 @@
 import os
-from dotenv import load_dotenv
 from vector_store import FaissVectorStore
 from langchain_groq import ChatGroq
 
@@ -21,8 +20,12 @@ class RAGSearch:
         else:
             self.vectorstore.load()
         
-        GROQ_API_KEY = os.getenv("GROQ_API_KEY")
-        self.llm = ChatGroq(groq_api_key=groq_api_key, model_name=llm_model)
+        GROQ_API_KEY = st.secrets["GROQ_API_KEY"]
+
+        self.llm = ChatGroq(
+            groq_api_key=GROQ_API_KEY,
+            model_name=llm_model
+        )
 
         print(f"[INFO] Groq LLM initialized: {llm_model}")
 
